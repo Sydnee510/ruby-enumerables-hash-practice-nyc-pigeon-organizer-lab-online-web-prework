807ca7e9d53490de1057a7a7a68a00f81cf9d819
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-12 03:31:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -38,3 +38,18 @@ def nyc_pigeon_organizer(data)
 }
 end
 
+def pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+end
